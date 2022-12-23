bb5986e95877247eca4b605188aad7104c96cb8c
yufiv <119109779+yufiv@users.noreply.github.com>
2022-12-23 17:44:22 +0400

    Add whiteBall.png

--- MACH.py
+++ MACH.py
@@ -11,9 +11,11 @@ class Ball(pygame.sprite.Sprite):
         # Вызываем конструктор самого класса Sprite
         super().__init__()
         # Загружаем изображение
-        self.image = pygame.image.load('ааа.png')
+        self.image = pygame.image.load('WhiteBall.png')
         # Настраиваем его. Не нужно здесь ничего менять, просто копируйте
-        self.image = self.image.convert()
+        # Здесь я поставил convert_alpha, чтобы правильно работала прозрачность
+        #self.image = self.image.convert()
+        self.image = self.image.convert_alpha()
         colorkey = self.image.get_at((0, 0))
         self.image.set_colorkey(colorkey)
         # Задаем размер. Первая 100 - ширина, вторая 100 - высота
@@ -23,22 +25,24 @@ class Ball(pygame.sprite.Sprite):
         self.rect.left = 270
         self.rect.top = 470
         self.s = random.randrange(1, 7)
+        self.rand = 0
+
     def update(self):
         self.rand = self.s
         if self.rand == 1:
-            self.rect.left-= 1
+            self.rect.left -= 1
             self.rect.top -= 1
         elif self.rand == 2:
-             self.rect.left-= 1
-        elif self.rand == 2:
+            self.rect.left -= 1
+        elif self.rand == 3:
             self.rect.left -= 1
             self.rect.top += 1
-        elif self.rand == 2:
+        elif self.rand == 4:
             self.rect.left += 1
             self.rect.top += 1
-        elif self.rand == 2:
+        elif self.rand == 5:
             self.rect.left += 1
-        elif self.rand == 2:
+        elif self.rand == 6:
             self.rect.left += 1
             self.rect.top -= 1
 
