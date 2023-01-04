dffa61c8bfa56b93c1d4b4a91df920a06657ea3c
88888888sasha <105802087+88888888sasha@users.noreply.github.com>
2023-01-04 13:25:07 +0400

    wd

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def kas():
 a = 0
 b = 0
 l = 1
+u = 0
 def draw_pole():
     pygame.draw.line(win, (250, 250, 250), (0, 0), (5, 1000), 20)
     pygame.draw.line(win, (250, 250, 250), (600, 0), (600, 1000), 20)
@@ -50,10 +51,16 @@ def draw_pole():
     pygame.draw.lines(win, ((250, 250, 250)), True, ((150, 50), (150, 0), (450, 0), (450, 50)), 10)
     pygame.draw.circle(win, ((250, 250, 250)), (300, 500), 40)
 def run():
-    if vrags.x > BAL.rect.left:
-        vrags.x -= 6
-    if vrags.x < BAL.rect.left:
-        vrags.x += 6
+    if BAL.rect.top > vrags.y:
+        if vrags.x > BAL.rect.left:
+            vrags.x -= 6
+        if vrags.x < BAL.rect.left:
+            vrags.x += 6
+    else:
+        if vrags.x > 250:
+            vrags.x -= 6
+        if vrags.x < 250:
+            vrags.x += 6
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -62,8 +69,8 @@ while True:
     win.fill((0, 225, 0))
 
 
-
     if l == 1:
+        u += 1
         draw_pole()
         all_sprites.update()
         vrags.draw()
@@ -92,16 +99,27 @@ while True:
             o = "You WIN!!!"
         if a >= 4:
             o = "You LOOSE!!!"
+        time = u // 60
+        c = "Заново"
+        z = ("YOU = " + str(b))
+        v = ("VRAG = " + str(a))
         pygame.draw.rect(win, (250, 250, 250), (0, 0, 600, 1000))
         pygame.draw.rect(win, (0, 250, 250), (0, 0, 600, 1000), 20)
         pygame.draw.rect(win, (0, 250, 0), (200, 700, 200, 126))
-        f1 = pygame.font.Font(None, 60)
+        n = ("time = " + str(time) + " second")
+        f1 = pygame.font.Font(None, 100)
+        f2 = pygame.font.Font(None, 60)
         text1 = f1.render(o, 1, (0, 0, 0))
-        win.blit(text1, (200, 200))
-        f1 = pygame.font.Font(None, 40)
-        c = "Заново"
-        text1 = f1.render(c, 1, (0, 0, 0))
-        win.blit(text1, (250, 750))
+        text2 = f2.render(c, 1, (0, 0, 0))
+        text3 = f2.render(z, 1, (0, 0, 0))
+        text4 = f2.render(v, 1, (0, 0, 0))
+        text5 = f2.render(n, 1, (0, 0, 0))
+        win.blit(text1, (100, 200))
+        win.blit(text2, (220, 750))
+        win.blit(text3, (160, 300))
+        win.blit(text4, (160, 400))
+        win.blit(text5, (160, 500))
+        print()
         l = 0
         p = pygame.mouse.get_pos()
         for i in pygame.event.get():
@@ -115,7 +133,9 @@ while True:
                 Player.y = 900
                 vrags.x = 250
                 vrags.y = 100
+                u = 0
 
 
     pygame.display.update()
     clock.tick(FPS)
+
